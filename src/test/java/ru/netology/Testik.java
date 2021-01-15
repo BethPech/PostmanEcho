0d1cd56fc5ac2e37c1f7c0910b26c48145305cba
BethPech <colnze17-94@mail.ru>
2021-01-15 23:03:55 +0300

    Test passed

---
+++ src/test/java/ru/netology/Testik.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+public class Testik {
+    @org.junit.jupiter.api.Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
+
